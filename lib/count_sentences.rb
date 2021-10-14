4119dc6d1922d24b9f4793677ffa53d4e4b9097a
cody <cmchaff14@gmail.com>
2021-10-14 09:53:21 -0500

    commit tree 0b19ccb2fe544c7193722b160168a63d1338ad54

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class String
-
+  attr_reader
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.count
+    
   end
 end
